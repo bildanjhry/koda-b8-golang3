d069be7005dd235580761970a8c4a4e41dac0dd6
Bildan Jauhary <bildanjauhary@gmail.com>
2026-07-06 16:52:04 +0700

    feat: add result of inserting new value in slice with slice partitioning method

---
+++ go.mod
@@ -0,0 +1,3 @@
+module koda-b8-golang3
+
+go 1.26.4
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	scores := []int{50, 75, 66, 20, 22, 32, 90}
+	length := len(scores)
+	mid := length / 2 // get the half of array length
+	left := []int{}
+	right := []int{}
+
+	left = scores[0:mid]
+	right = scores[mid+1 : length]
+
+	left = append(left, 88) // append new data
+
+	scores = append(left, right...) // reassign with new arrays
+	for x := range len(scores) {
+		fmt.Println(scores[x])
+	}
+
+}
